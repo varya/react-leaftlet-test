b89d83733748e2b740d69ab360a6929234a1c188
Varya Stepanova <mail@varya.me>
2015-11-22 11:36:42 +0200

    Marker rendered separately

--- src/index.js
+++ src/index.js
@@ -6,22 +6,29 @@ require("!style!css!./styles.css");
 
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const position = [51.505, -0.09];
+const center = [51.505, -0.09];
+
+const marker = (function() {
+  const position = [51.500, -0.08];
+  return (
+    <Marker position={position}>
+        <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+        </Popup>
+    </Marker>
+  );
+})()
 
 render(
   <div>
     <h1>Hello, world!</h1>
     <div className="map">
-        <Map center={position} zoom={13}>
+        <Map center={center} zoom={13}>
             <TileLayer
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={position}>
-            <Popup>
-                <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-            </Popup>
-            </Marker>
+            {marker}
         </Map>
     </div>
   </div>
