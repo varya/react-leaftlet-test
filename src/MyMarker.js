17d48b65e0d50db53ff7865dd7909eca30cb8c2c
Varya Stepanova <mail@varya.me>
2015-11-22 12:53:38 +0200

    add componentWillMount

--- src/MyMarker.js
+++ src/MyMarker.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import { Map, Marker, Popup, TileLayer, MapLayer } from 'react-leaflet';
 
 class MyMarker extends MapLayer {
+  componentWillMount() {
+    super.componentWillMount();
+    const { map, position, ...props } = this.props;
+  }
   render () {
     return (
         <Marker position={this.props.position}>
